305886d5548df1b616836ec54407852e7c09d86b
Rudher <rudrabhardwaj121201@gmail.com>
2024-05-01 11:16:26 +0530

    components-homepage

---
+++ src/pages/homePage/homePage.jsx
@@ -0,0 +1,16 @@
+import './homePage.scss';
+
+function HomePage(){
+  return(
+    <div className='homePage'>
+      <div className="textContainer">
+        text
+      </div>
+      <div className="imageContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
